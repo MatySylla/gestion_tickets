c3256d2d5fe00b96948cfaeaea8ad98d0fd3f435
MatySylla <maty.sylla11@unv-thies.sn>
2024-03-30 12:52:23 +0000

    modification titre

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo mmmmmm',
+      title: 'Gestion des tickets',
       theme: ThemeData(
         // This is the theme of your application.
         //
