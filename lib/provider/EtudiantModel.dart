a942a1a2a9deb970517dc2062cd706f46255681b
MatySylla <maty.sylla11@unv-thies.sn>
2024-04-03 11:16:32 +0000

    modification provider

--- lib/provider/EtudiantModel.dart
+++ lib/provider/EtudiantModel.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gestion_tickets/model/Etudiant.dart';
@@ -20,12 +22,14 @@ class EtudiantModel extends ChangeNotifier {
       // Vérifier si le document existe
       if (documentSnapshot.exists) {
         // Convertir les données Firestore en un objet Etudiant en utilisant la méthode fromJson
-        Map<String, dynamic> data                                                                                                                                                                                                                                                                                                                        =
-            documentSnapshot.data() as Map<String, dynamic>;
+        Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
         Etudiant etudiant = Etudiant.fromJson(data);
 
         // Mettre à jour l'étudiant dans le modèle
         _etudiant = etudiant;
+        if (data.containsKey('imageUrl')) {
+          _etudiant!.photoUrl = data['imageUrl']; // Ajouter l'URL de l'image
+        }
         notifyListeners(); // Notifie les écouteurs du changement
       } else {
         // Le document n'existe pas, vous pouvez gérer cette situation comme nécessaire
@@ -33,8 +37,7 @@ class EtudiantModel extends ChangeNotifier {
       }
     } catch (error) {
       // Gérer les erreurs de récupération des données depuis Firestore
-      print(
-          'Erreur lors de la récupération des données de l\'utilisateur : $error');
+      print('Erreur lors de la récupération des données de l\'utilisateur : $error');
     }
   }
 }
