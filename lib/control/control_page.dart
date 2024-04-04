99833373cd5e5e5a9305be86cc94ca6c00afb66d
MatySylla <maty.sylla11@unv-thies.sn>
2024-04-04 13:29:45 +0000

    modification contrlPage

--- lib/control/control_page.dart
+++ lib/control/control_page.dart
@@ -1,63 +1,136 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:gestion_tickets/screens/EtudiantHome.dart';
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
-class ControlePage extends StatelessWidget {
-  const ControlePage({super.key});
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainScreenState createState() => _MainScreenState();
+}
 
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const EtudiantHomePage(),
+    const BalanceView(),
+    const MenuView(),
+    const ReservationView(),
+    const PaymentView(),
+   
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(235, 225, 217, 217),
-          title: const Text('gestion des tickets', style: TextStyle(color: Colors.black)),
-          centerTitle: true,
-          elevation: 0, // Remove appbar shadow
-        ),
-        body:  const TabBarView(
-          children: [
-            // Replace with your TabBarView content
-           
-           EtudiantHomePage(),
-            Center(child: Text('Page2')),
-            Center(child: Text('Page3')),
-          ],
-        ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            color: Color.fromARGB(131, 255, 255, 255),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 10,
-                offset: const Offset(0, -3),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gestion des tickets'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
           ),
-          child: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home_outlined, size: 30),
-                text: 'Accueil',
-              ),
-              Tab(
-                icon: Icon(Icons.shopping_bag_outlined, size: 30),
-                text: 'Tickets',
-              ),
-              Tab(
-                icon: Icon(Icons.person_outline, size: 30),
-                text: 'Compte',
-              ),
-            ],
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey,
+            BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Solde',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant_menu),
+            label: 'Menus',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Réservation',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payment),
+            label: 'Paiement',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class BalanceView extends StatelessWidget {
+  const BalanceView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Solde de tickets : 10',
+        style: TextStyle(fontSize: 24.0),
+      ),
+    );
+  }
+}
+
+class MenuView extends StatelessWidget {
+  const MenuView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const <Widget>[
+        ListTile(
+          title: Text('Menu du jour 1'),
+          subtitle: Text('Description du menu'),
+          trailing: Text('Prix : \$5'),
         ),
+        ListTile(
+          title: Text('Menu du jour 2'),
+          subtitle: Text('Description du menu'),
+          trailing: Text('Prix : \$6'),
+        ),
+        // Ajoutez plus d'éléments de menu ici...
+      ],
+    );
+  }
+}
+
+class ReservationView extends StatelessWidget {
+  const ReservationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          // Logique pour réserver un repas
+        },
+        child: const Text('Réserver un repas'),
+      ),
+    );
+  }
+}
+
+class PaymentView extends StatelessWidget {
+  const PaymentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          // Logique pour effectuer un paiement
+        },
+        child: const Text('Effectuer un paiement'),
       ),
     );
   }
