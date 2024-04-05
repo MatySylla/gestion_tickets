e4b6ea21b7a0a9a4a4cb388c04e83be4f2a81d79
boussomdn <mdiarra.ndiaye1@univ-thies.sn>
2024-04-05 16:43:44 +0000

    ajout paiement

--- lib/widgets/paiement/PaymentView.dart
+++ lib/widgets/paiement/PaymentView.dart
@@ -1,17 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:gestion_tickets/widgets/paiement/paymentBloc.dart';
 
-class PaymentView extends StatelessWidget {
-  const PaymentView({super.key});
+
+class PaymentView extends StatefulWidget {
+  const PaymentView({Key? key}) : super(key: key);
+
+  @override
+  _PaymentViewState createState() => _PaymentViewState();
+}
+
+class _PaymentViewState extends State<PaymentView> {
+  final PaymentBloc _paymentBloc = PaymentBloc();
+  String _selectedPaymentMethod = '';
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          // Logique pour effectuer un paiement
-        },
-        child: const Text('Effectuer un paiement'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Choisissez le mode de paiement :',
+            style: TextStyle(fontSize: 18),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedPaymentMethod = 'Visa'; // Modifier selon votre logique
+                  });
+                },
+                icon: const Icon(Icons.credit_card), // Icône pour Visa
+                color: _selectedPaymentMethod == 'Visa' ? Colors.blue : Colors.grey,
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedPaymentMethod = 'PayPal'; // Modifier selon votre logique
+                  });
+                },
+                icon: const Icon(Icons.payment), // Icône pour PayPal
+                color: _selectedPaymentMethod == 'PayPal' ? Colors.blue : Colors.grey,
+              ),
+              // Ajoutez d'autres icônes pour les modes de paiement supplémentaires selon vos besoins
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_selectedPaymentMethod.isNotEmpty) {
+                _paymentBloc.initiatePayment(_selectedPaymentMethod);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Veuillez sélectionner un mode de paiement.'),
+                ));
+              }
+            },
+            child: const Text('Effectuer le paiement'),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _paymentBloc.dispose();
+    super.dispose();
   }
 }
--- lib/widgets/paiement/paymentBloc.dart
+++ lib/widgets/paiement/paymentBloc.dart
@@ -2,20 +2,31 @@ import 'dart:async';
 import 'dart:math';
 
 class PaymentBloc {
-  final _paymentController = StreamController<bool>();
+  final _paymentController = StreamController<void>();
 
-  Stream<bool> get paymentStream => _paymentController.stream;
+  Stream<void> get paymentStream => _paymentController.stream;
 
-  void initiatePayment() async {
+  void initiatePayment(String selectedPaymentMethod) async {
+  try {
     // Simule une attente de 2 secondes avant de terminer le paiement
     await Future.delayed(Duration(seconds: 2));
 
-    // Simule un paiement réussi dans 80% des cas, échoué dans les 20% restants
+    // Simulation de la logique de paiement réussi (par exemple, en supposant que le paiement réussit dans 80% des cas)
     final bool paymentSuccessful = Random().nextInt(10) < 8; // 80% de chance de réussite
 
-    // Envoie le résultat du paiement via le stream
-    _paymentController.sink.add(paymentSuccessful);
+    if (paymentSuccessful) {
+      // Le paiement a réussi, émettre un événement à travers le stream
+      _paymentController.sink.add(true);
+    } else {
+      // Le paiement a échoué, émettre un événement à travers le stream avec false ou gérer l'erreur autrement
+      _paymentController.sink.addError('Le paiement a échoué');
+    }
+  } catch (e) {
+    // En cas d'erreur lors du traitement du paiement, émettre un événement d'erreur à travers le stream
+    _paymentController.sink.addError('Une erreur est survenue lors du traitement du paiement : $e');
   }
+}
+
 
   void dispose() {
     _paymentController.close();
