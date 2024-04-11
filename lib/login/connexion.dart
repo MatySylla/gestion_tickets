0b0a7a60c70b7fae0caf09bde9acd0ca91b1e749
boussomdn <mdiarra.ndiaye1@univ-thies.sn>
2024-04-11 00:10:04 +0000

    changement login

--- lib/login/connexion.dart
+++ lib/login/connexion.dart
@@ -6,13 +6,38 @@ import 'package:gestion_tickets/login/inscription.dart';
 import 'package:gestion_tickets/provider/EtudiantModel.dart';
 import 'package:provider/provider.dart'; // Importez la page d'inscription
 
-// ignore: camel_case_types
-class Connexion extends StatelessWidget {
+
+class Connexion extends StatefulWidget {
+  const Connexion({Key? key}) : super(key: key);
+   @override
+  _ConnexionState createState() => _ConnexionState();
+}
+class _ConnexionState extends State<Connexion> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _obscureTextPswd = true; // Ajout de _obscureTextPswd pour le contrôle de visibilité du mot de passe
 
-  // ignore: use_key_in_widget_constructors
-  Connexion({Key? key});
+ 
+
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty || !RegExp(r"^[a-zA-Z0-9]+\.[a-zA-Z0-9]+@univ-thies\.sn$").hasMatch(value)) {
+      return 'Entrez un e-mail valide.';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Veuillez entrer un mot de passe';
+    }
+    
+    if (value.length != 11 ||  !RegExp(r'^[0-9]+$').hasMatch(value)) {
+      return 'Le mot de passe doit contenir exactement 11 chiffres';
+    }
+    return null;
+  }
 
   Future<void> _signInWithEmailAndPassword(BuildContext context) async {
     try {
@@ -25,7 +50,7 @@ class Connexion extends StatelessWidget {
 
       // Récupérer les informations de l'utilisateur connecté
       User? user = userCredential.user;
-      if (user != null) {
+      if (user != null && _formKey.currentState!.validate()) {
         // Mettre à jour les données de l'étudiant dans EtudiantModel
         // ignore: use_build_context_synchronously
         await Provider.of<EtudiantModel>(context, listen: false)
@@ -40,8 +65,7 @@ class Connexion extends StatelessWidget {
     } catch (e) {
       // En cas d'erreur d'authentification
       // ignore: use_build_context_synchronously
-          showErrorDialog(context, 'Erreur d\'authentification : $e'
-      );
+      showErrorDialog(context, 'Erreur d\'authentification : $e');
     }
   }
 
@@ -49,7 +73,9 @@ class Connexion extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
+      body: Form(
+        key: _formKey,
+      child: Column(
         children: [const SizedBox(height: 50),
           Image.asset(
             'assets/login.png'
@@ -62,36 +88,51 @@ class Connexion extends StatelessWidget {
 
           ),
           const SizedBox(height: 25),
-          TextField(
+          TextFormField(
             controller: _emailController,
             style: const TextStyle(fontSize: 15),
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
-                    ),
-                  ),
+            decoration: const InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
                 ),
-
+              ),
+            ),
+            validator: _validateEmail,
           ),
           const SizedBox(height: 16.0),
-          TextField(
+          TextFormField(
             controller: _passwordController,
             style: const TextStyle(fontSize: 15),
-                decoration: const InputDecoration(
-                  labelText: 'Mot de passe',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
-                    ),
-                  ),
+            decoration: InputDecoration(
+              labelText: 'Mot de passe',
+               suffixIcon: InkWell(
+                onTap: () {
+                  setState(() {
+                     _obscureTextPswd = !_obscureTextPswd;
+                  });
+                },
+                child: Icon(
+                  _obscureTextPswd ? Icons.visibility : Icons.visibility_off,
                 ),
-            obscureText: true,
+               ),
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
+                ),
+              ),
+            ),
+            obscureText: _obscureTextPswd,
+            validator: _validatePassword,
           ),
           const SizedBox(height: 16.0),
           ElevatedButton(
-            onPressed: () => _signInWithEmailAndPassword(context),
+            onPressed: () {
+              if (_formKey.currentState!.validate()){
+              _signInWithEmailAndPassword(context);
+              }
+            },
             child: const Text('Se connecter'),
           ),
           TextButton(
@@ -106,6 +147,7 @@ class Connexion extends StatelessWidget {
           ),
         ],
       ),
+    ),
     );
   }
 }
--- lib/login/inscription.dart
+++ lib/login/inscription.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gestion_tickets/compositions/messageErreur.dart';
@@ -8,8 +6,10 @@ import 'package:gestion_tickets/model/Etudiant.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
 
@@ -17,72 +17,46 @@ class SignUpPage extends StatelessWidget {
   SignUpPage({Key? key});
 
   Future<void> _signUpWithEmailAndPassword(BuildContext context) async {
-    String email = _emailController.text.trim();
-    String password = _passwordController.text.trim();
-    String firstName = _firstNameController.text.trim();
-    String lastName = _lastNameController.text.trim();
+    if (_formKey.currentState!.validate()) {
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+      String firstName = _firstNameController.text.trim();
+      String lastName = _lastNameController.text.trim();
 
-    // Vérifier si les champs sont vides
-    if (email.isEmpty ||
-        password.isEmpty ||
-        firstName.isEmpty ||
-        lastName.isEmpty) {
-          showErrorDialog(context, 'Veuillez remplir tous les champs.'
-      );
-      return;
-    }
-    // Valider l'adresse e-mail
-    if (!RegExp(r'^[a-zA-Z]+\.[a-zA-Z]+@univ-thies\.sn$').hasMatch(email)) {
-      (context,'Veuillez saisir votre adresse e-mail universitaire au format "@univ-thies.sn".'
-      );
-      return;
-    }
-    if (!RegExp(r'^\d{11}$').hasMatch(password)) {
-      showErrorDialog(context,
-          'Veuillez saisir une adresse e-mail au format "prenom.nom@univ-thies.sn".');
+      try {
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
 
-      return;
-    }
+        // Récupérer l'ID de l'utilisateur nouvellement créé
+        String userId = userCredential.user!.uid;
 
-    try {
-      UserCredential userCredential =
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+        // Enregistrer les informations supplémentaires dans la base de données
+        await saveUserData(userId, email, firstName, lastName);
 
-      // Récupérer l'ID de l'utilisateur nouvellement créé
-      String userId = userCredential.user!.uid;
-
-      // Enregistrer les informations supplémentaires dans la base de données
-      await saveUserData(userId);
-
-      // Une fois l'utilisateur inscrit, redirigez-le vers la page principale
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const MyApp()),
-      );
-    } catch (e) {
-      showErrorDialog(context, 'Erreur lors de l\'inscription : $e');
+        // Une fois l'utilisateur inscrit, redirigez-le vers la page principale
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const MyApp()),
+        );
+      } catch (e) {
+        showErrorDialog(context, 'Erreur lors de l\'inscription : $e');
+      }
     }
   }
 
-  Future<void> saveUserData(String userId) async {
-    // Récupérer le nom et le prénom saisis par l'utilisateur
-    String nom = _firstNameController.text.trim();
-    String prenom = _lastNameController.text.trim();
-    String email = _emailController.text.trim();
-
+  Future<void> saveUserData(
+      String userId, String email, String firstName, String lastName) async {
     Etudiant nouvelEtudiant = Etudiant(
       id: userId, // Utiliser l'ID de l'utilisateur comme identifiant de l'étudiant
       email: email,
-      nom: nom,
-      prenom: prenom,
+      nom: lastName,
+      prenom: firstName,
     );
 
     // Enregistrer les informations dans la base de données, par exemple Firestore
-    // Ici, nous supposons que vous avez une collection "users" dans Firestore
-    // où chaque document a l'ID de l'utilisateur comme ID du document
-    // Vous pouvez personnaliser cela selon votre structure de base de données
+    // Ici, nous supposons que vous avez une collection "etudiant" dans Firestore
     await FirebaseFirestore.instance.collection('etudiant').doc(userId).set(
           nouvelEtudiant.toJson(),
         );
@@ -91,31 +65,39 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Inscription'),
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+      appBar: AppBar(
+        title: const Text('Inscription'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset('assets/login.png'),
-                TextField(
+                TextFormField(
                   controller: _firstNameController,
                   style: const TextStyle(fontSize: 15),
                   decoration: const InputDecoration(
-                    labelText: 'Prenom',
+                    labelText: 'Prénom',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(
                         Radius.circular(50),
                       ),
                     ),
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez saisir votre prénom';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                TextField(
+                TextFormField(
                   controller: _lastNameController,
                   style: const TextStyle(fontSize: 15),
                   decoration: const InputDecoration(
@@ -126,9 +108,15 @@ class SignUpPage extends StatelessWidget {
                       ),
                     ),
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez saisir votre nom';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                TextField(
+                TextFormField(
                   controller: _emailController,
                   style: const TextStyle(fontSize: 15),
                   decoration: const InputDecoration(
@@ -139,9 +127,19 @@ class SignUpPage extends StatelessWidget {
                       ),
                     ),
                   ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        !RegExp(
+                            r'^[a-zA-Z]+\.[a-zA-Z]+@univ-thies\.sn$')
+                            .hasMatch(value)) {
+                      return 'Veuillez saisir votre adresse e-mail universitaire au format "prenom.nom@univ-thies.sn"';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                TextField(
+                TextFormField(
                   controller: _passwordController,
                   style: const TextStyle(fontSize: 15),
                   decoration: const InputDecoration(
@@ -153,6 +151,35 @@ class SignUpPage extends StatelessWidget {
                     ),
                   ),
                   obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez saisir votre mot de passe';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  style: const TextStyle(fontSize: 15),
+                  decoration: const InputDecoration(
+                    labelText: 'Confirmer le mot de passe',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez confirmer votre mot de passe';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Les mots de passe ne correspondent pas';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
@@ -162,6 +189,8 @@ class SignUpPage extends StatelessWidget {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
