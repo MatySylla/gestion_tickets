dcf5efd27b533c8922edeb08ca12074eeb59d77f
MatySylla <maty.sylla11@unv-thies.sn>
2024-04-11 20:21:53 +0000

    modification model ticket

--- lib/model/tickets.dart
+++ lib/model/tickets.dart
@@ -1,17 +1,42 @@
 class Ticket {
   final String id;
   final String userId;
-  final DateTime dateAchat; // Date d'achat automatiquement générée
-  final double montant; // Montant du ticket
-  final TypeTicket type; // Type de ticket
+  final DateTime dateAchat;
+  final int nombreTicketsPetitDej;
+  final int nombreTicketsRepas;
+  final TypeTicket type;
 
   Ticket({
     required this.id,
     required this.userId,
     required DateTime dateAchat,
-    required this.montant,
+    required this.nombreTicketsRepas,
+    required this.nombreTicketsPetitDej,
     required this.type,
-  }) : dateAchat = DateTime.now(); // Initialisation avec la date et l'heure actuelles
+  }): dateAchat =
+     DateTime.now();
+
+  factory Ticket.fromJson(Map<String, dynamic> json) {
+    return Ticket(
+      id: json['id'] ?? '',
+      userId: json['userId'] ?? '',
+      dateAchat: DateTime.parse(json['dateAchat'] ?? ''), // Convertir en DateTime
+      nombreTicketsRepas: json['nombre_tickets_repas'] ?? 0,
+      nombreTicketsPetitDej: json['nombre_tickets_petit_dej'] ?? 0,
+      type: _getTypeFromJson(json['type'] ?? ''),
+    );
+  }
+
+  static TypeTicket _getTypeFromJson(String type) {
+    switch (type) {
+      case 'repas':
+        return TypeTicket.repas;
+      case 'petitDejeuner':
+        return TypeTicket.petitDejeuner;
+      default:
+        return TypeTicket.repas;
+    }
+  }
 }
 
 // Enumération pour les types de tickets
