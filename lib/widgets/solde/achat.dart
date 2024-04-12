2da4985b1281ac8c7cb640400153f96c03b3e3d8
boussomdn <mdiarra.ndiaye1@univ-thies.sn>
2024-04-12 19:55:59 +0000

    ajout modes de paiements et quelques reajustements

--- lib/widgets/solde/achat.dart
+++ lib/widgets/solde/achat.dart
@@ -1,24 +1,35 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PageAchatTickets extends StatefulWidget {
-  const PageAchatTickets({super.key});
+  const PageAchatTickets({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _PageAchatTicketsState createState() => _PageAchatTicketsState();
 }
 
 class _PageAchatTicketsState extends State<PageAchatTickets> {
   int _nombreTicketsRepas = 0;
   int _nombreTicketsPetitDej = 0;
-  final double _prixTicketRepas = 100.0; // Prix unitaire du ticket repas
-  final double _prixTicketPetitDej = 50.0; // Prix unitaire du ticket petit déjeuner
-  double _prixTotal = 0.0; // Prix total initial
+  final double _prixTicketRepas = 100.0;
+  final double _prixTicketPetitDej = 50.0;
+  double _prixTotal = 0.0;
 
   final TextEditingController _controllerRepas = TextEditingController();
   final TextEditingController _controllerPetitDej = TextEditingController();
+
+  final List<String> _paymentOptionsImages = [
+    'assets/wave.jpeg',
+    'assets/om.jpeg',
+    'assets/fm.png',
+    'assets/ecobank.jpg',
+    'assets/sesapay.jpg',
+  ];
+
+  String _selectedPaymentOption = '';
 
   @override
   void initState() {
@@ -27,7 +38,6 @@ class _PageAchatTicketsState extends State<PageAchatTickets> {
     _controllerPetitDej.text = _nombreTicketsPetitDej.toString();
   }
 
-  // Méthode pour mettre à jour le prix total en fonction du nombre de tickets sélectionnés
   void _mettreAJourPrixTotal() {
     setState(() {
       _nombreTicketsRepas = int.parse(_controllerRepas.text);
@@ -37,22 +47,16 @@ class _PageAchatTicketsState extends State<PageAchatTickets> {
     });
   }
 
-  // Méthode pour effectuer le paiement fictif
   void _effectuerPaiement() {
-    // Ici, vous pouvez intégrer la logique de paiement réelle
-    // Pour l'exemple, je vais simplement afficher un message de confirmation
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('Paiement effectué'),
-          content: Text(
-              'Votre paiement de $_prixTotal fcf a été effectué avec succès.'),
+          content: Text('Votre paiement de $_prixTotal F CFA a été effectué avec succès.'),
           actions: [
             TextButton(
               onPressed: () {
-                
-                // Une fois le paiement effectué, enregistrer les tickets dans Firestore
                 _enregistrerTicketsFirestore();
                 Navigator.of(context).pop();
               },
@@ -65,99 +69,143 @@ class _PageAchatTicketsState extends State<PageAchatTickets> {
   }
 
   void _enregistrerTicketsFirestore() {
-  String userId = FirebaseAuth.instance.currentUser!.uid; // Récupérer l'ID de l'utilisateur connecté
+    String userId = FirebaseAuth.instance.currentUser!.uid;
 
-  FirebaseFirestore.instance.collection('tickets').add({
-    'user_id': userId, // Ajouter l'ID de l'utilisateur connecté
-    'nombreTicketsRepas': _nombreTicketsRepas,
-    'nombreTicketsPetitDej': _nombreTicketsPetitDej,
-    'prix_total': _prixTotal,
-    'timestamp': Timestamp.now(), // Ajouter un timestamp pour enregistrer l'heure de l'achat
-  }).then((value) {
-    // Afficher un message pour indiquer que les tickets ont été enregistrés avec succès
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Achat réussi'),
-          content: const Text('Vos tickets ont été enregistrés avec succès.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }).catchError((error) {
-    // En cas d'erreur lors de l'enregistrement, afficher un message d'erreur
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Erreur'),
-          content: Text('Une erreur s\'est produite lors de l\'enregistrement des tickets : $error'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  });
-}
+    FirebaseFirestore.instance.collection('tickets').add({
+      'user_id': userId,
+      'nombreTicketsRepas': _nombreTicketsRepas,
+      'nombreTicketsPetitDej': _nombreTicketsPetitDej,
+      'prix_total': _prixTotal,
+      'timestamp': Timestamp.now(),
+    }).then((value) {
+      
+    }).catchError((error) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Erreur'),
+            content: Text('Une erreur s\'est produite lors de l\'enregistrement des tickets : $error'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Acheter des Tickets'),
+        title: const Text('Achat Tickets'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _controllerRepas,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Tickets Repas',
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                'assets/payer.jpeg',
+                height: 100,
+                width: 100,
+                fit: BoxFit.contain,
               ),
-              onChanged: (_) => _mettreAJourPrixTotal(),
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              controller: _controllerPetitDej,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Tickets Petit Déj',
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: _controllerRepas,
+                keyboardType: TextInputType.number,
+                style: const TextStyle(fontSize: 15),
+                decoration: const InputDecoration(
+                  labelText: 'Tickets repas',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  ),
+                ),
+                onChanged: (_) => _mettreAJourPrixTotal(),
               ),
-              onChanged: (_) => _mettreAJourPrixTotal(),
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              'Prix total: $_prixTotal fcf',
-              style: const TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Intégrer ici le processus de paiement
-                _effectuerPaiement();
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: _controllerPetitDej,
+                keyboardType: TextInputType.number,
+                style: const TextStyle(fontSize: 15),
+                decoration: const InputDecoration(
+                  labelText: 'Tickets petit dèj',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  ),
+                ),
+                onChanged: (_) => _mettreAJourPrixTotal(),
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                'Prix total: $_prixTotal F CFA',
+                style: const TextStyle(fontSize: 20.0),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Choisir un mode de paiement'),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            children: List.generate((_paymentOptionsImages.length / 2).ceil(), (index) {
+                              return Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  for (int i = index * 2; i < min(index * 2 + 2, _paymentOptionsImages.length); i++)
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedPaymentOption = 'Mode de paiement ${i + 1}';
+                                        });
+                                        Navigator.of(context).pop();
+                                        _effectuerPaiement();
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: SizedBox(
+                                          width: 50.0,
+                                          height: 50.0,
+                                          child: Image.asset(
+                                            _paymentOptionsImages[i],
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              );
+                            }),
+                          ),
+                      ),
+                    );
+                  },
+                );
               },
-              child: const Text('Payer et acheter maintenant'),
+              child: const Text('Acheter'),
             ),
+            const SizedBox(height: 20.0),
+            if (_selectedPaymentOption.isNotEmpty)
+              Text('Mode de paiement sélectionné: $_selectedPaymentOption'),
           ],
         ),
       ),
+      )
     );
   }
 }
