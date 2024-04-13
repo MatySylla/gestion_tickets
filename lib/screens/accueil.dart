a5544bbfad102c0e030dcda046c4faa5fad6a90c
boussomdn <mdiarra.ndiaye1@univ-thies.sn>
2024-04-13 18:49:33 +0000

    reajustement de l'accueil

--- lib/screens/accueil.dart
+++ lib/screens/accueil.dart
@@ -1,66 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:gestion_tickets/provider/EtudiantModel.dart';
+import 'package:provider/provider.dart';
 
 class AccueilPage extends StatelessWidget {
-  const AccueilPage({Key? key}) : super(key: key);
+  const AccueilPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Accueil'),
-        backgroundColor: Colors.transparent, // Définit la barre d'application transparente
-        elevation: 0, // Supprime l'ombre de la barre d'application
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.grey[300], // Couleur de fond grise
-                borderRadius: BorderRadius.circular(20.0), // Coins arrondis
+      body: Consumer<EtudiantModel>(
+        builder: (context, etudiantModel, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                color: Colors.white,
+                child: etudiantModel.etudiant != null
+                    ? Text(
+                        'Bienvenue ${etudiantModel.etudiant!.prenom} ${etudiantModel.etudiant!.nom} !',
+                        style: const TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.deepPurple,
+                        ),
+                      )
+                    : const Text(
+                        'Chargement...',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
               ),
-              child: const Text(
-                'Bienvenue dans notre application',
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ElevatedButton(
+                onPressed: () {
+                  // Action à effectuer lors de l'appui sur le bouton "Réserver un ticket"
+                },
+                child: const Text('Réserver un ticket'),
               ),
-            ),
-            SizedBox(height: 20.0), // Espace entre le titre et les cartes
-            _buildCard('assets/e-tick.jpeg', 'Description de l\'annonce 1'),
-            _buildCard('assets/e-tick.jpeg', 'Description de l\'annonce 2'),
-            SizedBox(height: 20.0), // Espace entre les annonces et les mises à jour
-            _buildCard('assets/e-tick.jpeg', 'Description de la mise à jour 1'),
-            _buildCard('assets/e-tick.jpeg', 'Description de la mise à jour 2'),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildCard(String  imagePath, String subtitle) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Card(
-        elevation: 3, // Ajoute une ombre légère
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)), // Coins arrondis
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(
-              imagePath,
-              fit: BoxFit.cover, // Ajuste l'image pour remplir le widget
-            ),
-            ListTile(
-              contentPadding: const EdgeInsets.all(15.0),
-              title: Text(
-                imagePath.contains('annonce') ? 'Annonce' : 'Mise à jour',
-                style: const TextStyle(fontWeight: FontWeight.bold),
+              ElevatedButton(
+                onPressed: () {
+                  // Action à effectuer lors de l'appui sur le bouton "Acheter des tickets"
+                },
+                child: Text('Acheter des tickets'),
               ),
-              subtitle: Text(subtitle),
-            ),
-          ],
-        ),
+              ElevatedButton(
+                onPressed: () {
+                  // Action à effectuer lors de l'appui sur le bouton "Consulter le solde"
+                },
+                child: Text('Consulter le solde'),
+              ),
+              // Autres fonctionnalités ou sections de la page d'accueil
+            ],
+          );
+        },
       ),
     );
   }
