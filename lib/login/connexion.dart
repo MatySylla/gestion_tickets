89cef7298cf23069be3c6c4405a849d7ae0ed636
boussomdn <mdiarra.ndiaye1@univ-thies.sn>
2024-04-14 15:32:42 +0000

    changement fonction signInWithEmailAndPassword

--- lib/login/connexion.dart
+++ lib/login/connexion.dart
@@ -4,12 +4,13 @@ import 'package:gestion_tickets/compositions/messageErreur.dart';
 import 'package:gestion_tickets/control/control_page.dart';
 import 'package:gestion_tickets/login/ResetPasswordPage.dart';
 import 'package:gestion_tickets/login/inscription.dart';
+import 'package:gestion_tickets/portier/admin.dart';
 import 'package:gestion_tickets/provider/EtudiantModel.dart';
 import 'package:provider/provider.dart'; // Importez la page d'inscription
 
 class Connexion extends StatefulWidget {
   const Connexion({Key? key}) : super(key: key);
-   @override
+  @override
   _ConnexionState createState() => _ConnexionState();
 }
 
@@ -17,10 +18,14 @@ class _ConnexionState extends State<Connexion> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  bool _obscureTextPswd = true; // Ajout de _obscureTextPswd pour le contrôle de visibilité du mot de passe
+  bool _obscureTextPswd =
+      true; // Ajout de _obscureTextPswd pour le contrôle de visibilité du mot de passe
 
   String? _validateEmail(String? value) {
-    if (value == null || value.isEmpty || !RegExp(r"^[a-zA-Z0-9]+\.[a-zA-Z0-9]+@univ-thies\.sn$").hasMatch(value)) {
+    if (value == null ||
+        value.isEmpty ||
+        !RegExp(r"^[a-zA-Z0-9]+\.[a-zA-Z0-9]+@univ-thies\.sn$")
+            .hasMatch(value)) {
       return 'Entrez un e-mail valide.';
     }
     return null;
@@ -30,8 +35,8 @@ class _ConnexionState extends State<Connexion> {
     if (value == null || value.isEmpty) {
       return 'Veuillez entrer un mot de passe';
     }
-    
-    if (value.length != 11 ||  !RegExp(r'^[0-9]+$').hasMatch(value)) {
+
+    if (value.length != 11 || !RegExp(r'^[0-9]+$').hasMatch(value)) {
       return 'Le mot de passe doit contenir exactement 11 chiffres';
     }
     return null;
@@ -53,10 +58,18 @@ class _ConnexionState extends State<Connexion> {
         await Provider.of<EtudiantModel>(context, listen: false)
             .fetchEtudiantDataFromFirestore(user.uid);
 
-        // Rediriger vers la page d'accueil de l'étudiant
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) =>  const MainScreen(),
-        ));
+        // Vérifier l'email de l'utilisateur pour décider de la redirection
+        if (_emailController.text.trim() == 'mdiarra.ndiaye1@univ-thies.sn') {
+          // Rediriger vers la page de débit de tickets
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => PageDebitTickets(),
+          ));
+        } else {
+          // Rediriger vers la page d'accueil de l'étudiant
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => const MainScreen(),
+          ));
+        }
       }
     } catch (e) {
       // En cas d'erreur d'authentification
@@ -67,98 +80,100 @@ class _ConnexionState extends State<Connexion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              Image.asset('assets/login.png'),
-              const Text(
-                "Bienvenue",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 183, 179, 179),
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 25),
-              TextFormField(
-                controller: _emailController,
-                style: const TextStyle(fontSize: 15),
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
-                    ),
+        backgroundColor: Colors.white,
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                Image.asset('assets/login.png'),
+                const Text(
+                  "Bienvenue",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 183, 179, 179),
+                    fontSize: 16,
                   ),
                 ),
-                validator: _validateEmail,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                controller: _passwordController,
-                style: const TextStyle(fontSize: 15),
-                decoration: InputDecoration(
-                  labelText: 'Mot de passe',
-                  suffixIcon: InkWell(
-                    onTap: () {
-                      setState(() {
-                        _obscureTextPswd = !_obscureTextPswd;
-                      });
-                    },
-                    child: Icon(
-                      _obscureTextPswd ? Icons.visibility : Icons.visibility_off,
+                const SizedBox(height: 25),
+                TextFormField(
+                  controller: _emailController,
+                  style: const TextStyle(fontSize: 15),
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
                     ),
                   ),
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
-                    ),
-                  ),
+                  validator: _validateEmail,
                 ),
-                obscureText: _obscureTextPswd,
-                validator: _validatePassword,
-              ),
-              const SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _passwordController,
+                  style: const TextStyle(fontSize: 15),
+                  decoration: InputDecoration(
+                    labelText: 'Mot de passe',
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _obscureTextPswd = !_obscureTextPswd;
+                        });
+                      },
+                      child: Icon(
+                        _obscureTextPswd
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                    ),
+                  ),
+                  obscureText: _obscureTextPswd,
+                  validator: _validatePassword,
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
                     TextButton(
                       onPressed: () {
                         // Naviguer vers la page de réinitialisation de mot de passe
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => ResetPasswordPage()),
+                          MaterialPageRoute(
+                              builder: (context) => ResetPasswordPage()),
                         );
                       },
                       child: const Text('Mot de passe oublié ?'),
                     ),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()){
-                    _signInWithEmailAndPassword(context);
-                  }
-                },
-                child: const Text('Se connecter'),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Naviguer vers la page d'inscription
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()),
-                  );
-                },
-                child: const Text('Créer un compte'),
-              ),
-            ],
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _signInWithEmailAndPassword(context);
+                    }
+                  },
+                  child: const Text('Se connecter'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Naviguer vers la page d'inscription
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                    );
+                  },
+                  child: const Text('Créer un compte'),
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
