d3812602e0123fec20d68189a8ff62e78551a602
MatySylla <maty.sylla11@unv-thies.sn>
2024-04-18 15:30:48 +0000

    modification page solde

--- lib/widgets/solde/pageSolde.dart
+++ lib/widgets/solde/pageSolde.dart
@@ -14,63 +14,107 @@ class PageSoldeTickets extends StatelessWidget {
         child: Consumer<EtudiantModel>(
           builder: (context, etudiantModel, _) {
             if (etudiantModel.etudiant != null) {
-              // Récupérer les tickets repas et petit déjeuner de l'étudiant depuis le modèle
-              Future<num?> nombreTicketsRepas = etudiantModel.fetchSoldeTicketsRepasFromFirestore(etudiantModel.etudiant!.id);
-              Future<num?> nombreTicketsPetitDej = etudiantModel.fetchSoldeTicketsPetitDejFromFirestore(etudiantModel.etudiant!.id);
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Informations Personnelles:',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  _buildPersonalInfo('Nom:', etudiantModel.etudiant!.prenom),
+                  _buildPersonalInfo('Prénom:', etudiantModel.etudiant!.nom),
+                  _buildPersonalInfo('Email:', etudiantModel.etudiant!.email),
+                  SizedBox(height: 20),
+                  Text(
+                    'Solde des Tickets',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  FutureBuilder<num?>(
+                    future: etudiantModel.fetchSoldeTicketsRepasFromFirestore(etudiantModel.etudiant!.id),
+                    builder: (context, snapshotRepas) {
+                      return FutureBuilder<num?>(
+                        future: etudiantModel.fetchSoldeTicketsPetitDejFromFirestore(etudiantModel.etudiant!.id),
+                        builder: (context, snapshotPetitDej) {
+                          if (snapshotRepas.connectionState == ConnectionState.waiting ||
+                              snapshotPetitDej.connectionState == ConnectionState.waiting) {
+                            return Center(child: CircularProgressIndicator());
+                          } else if (snapshotRepas.hasError || snapshotPetitDej.hasError) {
+                            return Center(child: Text('Erreur de chargement des données'));
+                          } else {
+                            num soldeTotalTickets = (snapshotRepas.data ?? 0) + (snapshotPetitDej.data ?? 0);
 
-              return FutureBuilder<num?>(
-                future: nombreTicketsRepas,
-                builder: (context, snapshotRepas) {
-                  return FutureBuilder<num?>(
-                    future: nombreTicketsPetitDej,
-                    builder: (context, snapshotPetitDej) {
-                      if (snapshotRepas.connectionState == ConnectionState.waiting ||
-                          snapshotPetitDej.connectionState == ConnectionState.waiting) {
-                        // Afficher un indicateur de chargement si les données sont en cours de chargement
-                        return Center(child: CircularProgressIndicator());
-                      } else if (snapshotRepas.hasError || snapshotPetitDej.hasError) {
-                        // Afficher un message d'erreur si une erreur s'est produite lors de la récupération des données
-                        return Center(child: Text('Erreur de chargement des données'));
-                      } else {
-                        // Calculer le solde total des tickets
-                        num soldeTotalTickets = (snapshotRepas.data ?? 0) + (snapshotPetitDej.data ?? 0);
-
-                        // Afficher le solde des tickets dans l'interface utilisateur
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              'Solde des Tickets',
-                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              'Tickets Repas: ${snapshotRepas.data ?? 0}',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            Text(
-                              'Tickets Petit Déj: ${snapshotPetitDej.data ?? 0}',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              'Solde Total: $soldeTotalTickets',
-                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        );
-                      }
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    _buildTicketItem('Tickets Repas', snapshotRepas.data ?? 0, Colors.blue),
+                                    _buildTicketItem('Tickets Petit Déj', snapshotPetitDej.data ?? 0, Colors.green),
+                                  ],
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'Solde Total: $soldeTotalTickets',
+                                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            );
+                          }
+                        },
+                      );
                     },
-                  );
-                },
+                  ),
+                ],
               );
             } else {
-              // Si l'étudiant n'est pas disponible, affichez un message d'attente
               return Center(child: CircularProgressIndicator());
             }
           },
         ),
+      ),
+    );
+  }
+
+  Widget _buildPersonalInfo(String title, String value) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(width: 5),
+        Text(
+          value,
+          style: TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTicketItem(String title, num amount, Color color) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: color),
+          ),
+          SizedBox(height: 5),
+          Text(
+            '$amount',
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
       ),
     );
   }
