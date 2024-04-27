bb8f8508570c1534aa8be90dfa4db5d9307792a7
MatySylla <maty.sylla11@unv-thies.sn>
2024-04-27 14:51:26 +0000

    modification appbar

--- lib/control/control_page.dart
+++ lib/control/control_page.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gestion_tickets/control/profil.dart';
+import 'package:gestion_tickets/provider/EtudiantModel.dart';
 import 'package:gestion_tickets/screens/accueil.dart';
 import 'package:gestion_tickets/screens/home_page.dart';
 import 'package:gestion_tickets/widgets/solde/achat.dart';
 import 'package:gestion_tickets/widgets/menu/menu.dart';
 import 'package:gestion_tickets/widgets/historique/transaction.dart';
 import 'package:gestion_tickets/widgets/solde/pageSolde.dart';
+import 'package:provider/provider.dart';
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -39,7 +41,7 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         elevation: 0,
-        title: const Text('Gestion des tickets'),
+        title: const Text('E-CROUS', style:TextStyle(color: Colors.white) ),
         leading: IconButton(
           onPressed: () {
             // Action lorsque l'utilisateur appuie sur l'icône de menu
@@ -59,55 +61,64 @@ class _MainScreenState extends State<MainScreen> {
             },
             icon: const Icon(Icons.notifications, color: Colors.white),
           ),
-           PopupMenuButton<String>(
-      onSelected: (String value) {
-        // Logique pour chaque élément sélectionné dans le menu déroulant
-        switch (value) {
-          case 'Profil':
-            Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) =>  MyApp()),
-          );
-            break;
-          case 'Paramètres':
-            print('Paramètres utilisateur');
-            break;
-          case 'Déconnexion':
-            // Déconnexion de l'utilisateur et retour à la page d'accueil
-            FirebaseAuth.instance.signOut();
-            Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-            break;
-        }
-      },
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-        const PopupMenuItem<String>(
-          value: 'Profil',
-         child: ListTile(
-            leading: Icon(Icons.account_circle, color: Colors.deepPurple), // Icône Profil en mauve
-            title: Text('Profil'),
-          ),
+         PopupMenuButton<String>(
+  onSelected: (String value) {
+    // Logique pour chaque élément sélectionné dans le menu déroulant
+    switch (value) {
+      case 'Profil':
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyApp()), // Redirige vers la page de profil
+        );
+        break;
+      case 'Paramètres':
+        print('Paramètres utilisateur');
+        break;
+      case 'Déconnexion':
+        // Déconnexion de l'utilisateur et retour à la page d'accueil
+        FirebaseAuth.instance.signOut();
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        break;
+    }
+  },
+  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+    PopupMenuItem<String>(
+      value: 'Profil',
+      child: ListTile(
+        leading: Icon(Icons.account_circle, color: Colors.deepPurple), // Icône Profil en mauve
+        title: Consumer<EtudiantModel>(
+          builder: (context, etudiantModel, child) {
+            if (etudiantModel.etudiant != null) {
+              // Utilisez les informations de l'utilisateur pour créer le texte du menu
+              return Text('${etudiantModel.etudiant!.nom} ${etudiantModel.etudiant!.prenom}');
+            } else {
+              return Text('Profil');
+            }
+          },
         ),
-        const PopupMenuItem<String>(
-          value: 'Paramètres',
-         child: ListTile(
-            leading: Icon(Icons.settings, color: Colors.deepPurple), // Icône Paramètres en mauve
-            title: Text('Paramètres'),
-          )
-        ),
-        const PopupMenuItem<String>(
-          value: 'Déconnexion',
-          child: ListTile(
-            leading: Icon(Icons.logout, color: Colors.deepPurple), // Icône Déconnexion en mauve
-            title: Text('Déconnexion'),
-          ),
-        ),
-      ],
-      icon: const Icon(Icons.account_circle, color: Colors.white), // Icône en blanc
+      ),
     ),
+    const PopupMenuItem<String>(
+      value: 'Paramètres',
+      child: ListTile(
+        leading: Icon(Icons.settings, color: Colors.deepPurple), // Icône Paramètres en mauve
+        title: Text('Paramètres'),
+      )
+    ),
+    const PopupMenuItem<String>(
+      value: 'Déconnexion',
+      child: ListTile(
+        leading: Icon(Icons.logout, color: Colors.deepPurple), // Icône Déconnexion en mauve
+        title: Text('Déconnexion'),
+      ),
+    ),
+  ],
+  icon: const Icon(Icons.account_circle, color: Colors.white), // Icône en blanc
+),
+// Ajoutez d'autres éléments d'en-tête selon vos besoins     
         ],
-        // Ajoutez d'autres éléments d'en-tête selon vos besoins     
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
